8eb7b601c7c87bb91a312843b9389f78c5a9b659
Test <160234131+UserTestM@users.noreply.github.com>
2024-03-13 09:06:41 +0100

    Add files via upload

---
+++ Scripts/New Test Case (3)/Script1710261054994.groovy
@@ -0,0 +1,41 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startExistingApplication('com.example.myapplication')
+
+Mobile.pressBack()
+
+Mobile.pressBack()
+
+Mobile.pressBack()
+
+Mobile.pressBack()
+
+Mobile.verifyElementExist(findTestObject('Object Repository/New Folder (1)/android.widget.TextView - Chrome'), 0)
+
+Mobile.verifyElementExist(findTestObject('Object Repository/New Folder (1)/android.widget.TextView - Gmail'), 0)
+
+Mobile.verifyElementExist(findTestObject('Object Repository/New Folder (1)/android.widget.TextView - Photos'), 0)
+
+Mobile.tap(findTestObject('Object Repository/New Folder (1)/android.widget.TextView - Chrome'), 0)
+
+Mobile.takeScreenshot('C:\\Users\\MMARTI~1.LOS\\AppData\\Local\\Temp\\screenshot1031205517073829723.png')
+
+Mobile.closeApplication()
+
